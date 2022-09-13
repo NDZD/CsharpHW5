8c7c2c61591c95c6d8fbd2a526d567d4390791b2
ndzd <n_level@mail.ru>
2022-09-13 07:58:32 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+//которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+int GetEvenNumbers(int n){
+Random rnd = new Random();
+int count = 0;
+int [] arr = new int [n];
+for (int i = 0; i < n; i++){
+
+arr [i] = rnd.Next(100, 1000);
+}
+foreach (var item in arr)
+{
+    if(item % 2 == 0){
+        count++;
+    }
+    System.Console.WriteLine(item);     //проверка, вывод всех чисел массива
+}
+return count;
+}
+System.Console.WriteLine(GetEvenNumbers(10));
